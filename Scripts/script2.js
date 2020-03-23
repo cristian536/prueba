28ff1c5e7f28192f8ef30517d3b47fce8abf72cd
cristian536 <cdllanosp@libertadores.edu.co>
2020-03-22 23:12:16 -0500

    nuevo

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -1,16 +1,16 @@
-var puntajeEquipo1 = document.getElementById('score1');
-var puntajeEquipo2 = document.getElementById('score2');
+var puntajeColombia = document.getElementById('score1');
+var puntajeArgentina = document.getElementById('score2');
 var puntajeGanar = document.getElementById('puntajeGanar');
-var bEquipo1 = document.getElementById('bEquipo1');
-var bEquipo2 = document.getElementById('bEquipo2');
+var bColombia = document.getElementById('bColombia');
+var bArgentina = document.getElementById('bArgentina');
 var bReset = document.getElementById('reset');
 var gameOver = false;
 
 bEquipo1.addEventListener('click', function() {
   if (!gameOver) {
-    puntajeEquipo1.innerText = +puntajeEquipo1.innerText + 1;
-    if (+puntajeEquipo1.innerText == +puntajeGanar.innerText) {
-      puntajeEquipo1.classList.add('ganador');
+    puntajeColombia.innerText = +Colombia.innerText + 1;
+    if (+puntajeColombia.innerText == +puntajeGanar.innerText) {
+      puntajeColombia.classList.add('ganador');
       gameOver = true;
     }
   }
@@ -18,20 +18,20 @@ bEquipo1.addEventListener('click', function() {
 
 bEquipo2.addEventListener('click', function() {
   if (!gameOver) {
-    puntajeEquipo2.innerText = +puntajeEquipo2.innerText + 1;
-    if (+puntajeEquipo2.innerText == +puntajeGanar.innerText) {
-      puntajeEquipo2.classList.add('ganador');
+    puntajeArgentina.innerText = +puntajeArgentina.innerText + 1;
+    if (+puntajeArgentina.innerText == +puntajeGanar.innerText) {
+      puntajeArgentina.classList.add('ganador');
       gameOver = true;
     }
   }
 });
 
 bReset.addEventListener('click', function() {
-  puntajeEquipo1.innerText = 0;
-  puntajeEquipo2.innerText = 0;
+  puntajeColombia.innerText = 0;
+  puntajeArgentina.innerText = 0;
   puntajeGanar.innerText = 10;
-  puntajeEquipo1.classList.remove('ganador');
-  puntajeEquipo2.classList.remove('ganador');
+  puntajeColombia.classList.remove('ganador');
+  puntajeArgentina.classList.remove('ganador');
   puntajeGanar.classList.remove('ganador');
   gameOver = false;
 });
